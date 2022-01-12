28dd0105ea28007a42245667c180a28bdfd62bab
Josep Seto <jsetojseto@gmail.com>
2022-01-13 00:26:19 +0100

    removed unused code

--- src/crud-controller.ts
+++ src/crud-controller.ts
@@ -1,5 +1,4 @@
 import { Callback, Persistent, Model, Observable } from 'entropic-bond'
-type ControllerFactory = ( document: Persistent ) => CrudController<Persistent>
 
 interface ProgressStage {
 	name: string
@@ -30,18 +29,6 @@ export abstract class CrudController<T extends Persistent> {
 	
 	allRequiredPropertiesFilled(): boolean {
 		return true
-	}
-
-	static registerController( documentName: string, construct: ControllerConstructor ) {
-		this._factories[ documentName ] = ( document: Persistent ) => {
-			return new construct( document )
-		}
-	}
-
-	static createController( document: Persistent ) {
-		if ( !this._factories[ document.className ] ) throw new Error( `You should register ${ document.className } controller prior to use in the CRUD system`)
-		const factory = this._factories[ document.className ]
-		return factory( document )
 	}
 	
 	onChange( observer: Callback<CrudControllerEvent<T>> ) {
@@ -144,22 +131,6 @@ export abstract class CrudController<T extends Persistent> {
 	private _onChange: Observable<CrudControllerEvent<T>>
 	private _model: Model<T>
 	private _document: T
-	private static _factories: {[ documentName: string ]: ControllerFactory } = {}
 	private _progressStage: { [stageId: string]: ProgressStage } = {}
 	private _onProgress: Observable<ProgressEvent>
-}
-
-type ControllerConstructor = new ( document: Persistent ) => CrudController<Persistent>
-
-/**
- * Decorator to associate this controller with the document type that it manages.
- * This will allow to create a controller from a given document automatically.
- * 
- * @param documentTypeName the document type name that this controller is 
- * dessigned for.
- */
-export function controllerFor( documentTypeName: string ) {
-	return function( constructor: ControllerConstructor ) {
-		CrudController.registerController( documentTypeName, constructor )
-	}
 }
