60c0d5154228dd586cb3ef3f1eca177843d5c5c0
jseto <jsetojseto@gmail.com>
2023-10-09 16:53:06 +0700

    remove jest ans jsdom

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
 		globals: true,
-		environment: 'jsdom',
+		environment: 'happy-dom',
 		setupFiles: './add-ons/setup-tests.ts',
 		exclude: ['**/node_modules', '**/dist', '.idea', '.git', '.cache','**/lib', '**/out'],
 	},
